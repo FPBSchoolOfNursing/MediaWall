c3861ee3f83ab83f9af964beeca121e389af4449
Ben Margevicius <bdm4@case.edu>
2016-12-12 12:47:15 -0500

    Stored Procedures working

--- src/MediaWall/Controllers/DirectoryController.cs
+++ src/MediaWall/Controllers/DirectoryController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MediaWall.DAL;
+using MediaWall.DAL.Interfaces;
+using MediaWall.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,9 +14,22 @@ namespace MediaWall.Controllers
 {
     public class DirectoryController : Controller
     {
+        private DirectoryContext db;
+
+        public DirectoryController(DirectoryContext context)
+        {
+            db = context;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
+            var data = db.Set<GetDirectoryHeadersResult>().FromSql("Web.GetDirectoryHeaders @SearchTerm = {0}, @WhatDirectory = {1}", null, null);
+            
+            foreach(var d in data)
+            {
+                var s = d.Name;
+            }
+
             return View();
         }
     }
--- src/MediaWall/DAL/DirectoryContext.cs
+++ src/MediaWall/DAL/DirectoryContext.cs
@@ -1,13 +1,13 @@
 ﻿using MediaWall.DAL.Interfaces;
 using Microsoft.EntityFrameworkCore;
-
+using MediaWall.Models;
 
 namespace MediaWall.DAL
 {
     public class DirectoryContext : DbContext, IDirectoryContext
     {
-        public DirectoryContext(DbContextOptions<DirectoryContext> options) : base(options)
-        {
-        }        
+        public DirectoryContext(DbContextOptions<DirectoryContext> options) : base(options) { }
+
+        public virtual DbSet<GetDirectoryHeadersResult> GetDirectoryHeadersResult { get; set; }
     }
 }
